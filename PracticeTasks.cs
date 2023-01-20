9e5579762cefb40cda22f01491e9baac3bdba7be
OlliRepo <122178992+OpaFin@users.noreply.github.com>
2023-01-21 00:45:54 +0200

    PracticeGit 20.1

--- PracticeTasks.cs
+++ PracticeTasks.cs
@@ -364,18 +364,19 @@ namespace PracticeNameSpace
             Square
         }
 
-        /*Ouch! On your travels you were caught by a speed camera. 
+        /*Ouch! On your travels you were caught in a speed camera. 
         It's a demerit point for each 5km\h of speed over the speed limit.
         For 12 or more demerit points, you lose your lisence.
         Did you go fast, or did you go slow? Sometimes it doesnt' really matter.
         The speed limit is randomly set by the local authorities.*/
-
-        public static void SpeedCamera()
+        public static void CaughtInASpeedCamera()
         {
             ToughCop copBob = new ToughCop();
             Console.WriteLine("How fast did you drive?");
-            copBob.carSpeed = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(copBob.LayDownTheLaw());
+            var fumblingWords = Console.ReadLine();
+            var factualCarSpeed = Convert.ToInt32(fumblingWords);
+            var theFinalJudgement = copBob.LayDownTheLaw(factualCarSpeed);
+            Console.WriteLine(theFinalJudgement);
         }
         public class ToughCop
         {
@@ -384,21 +385,18 @@ namespace PracticeNameSpace
                 Random random = new Random();
                 speedLimit = random.Next(30, 120);
             }
-            public int speedLimit { get; }
-            public int carSpeed
-            { get; set; }
+            private int speedLimit;
             const int excessSpeedPerDemerit = 5;
-
-            public string LayDownTheLaw()
+            public string LayDownTheLaw(int carSpeed)
             {
-                int demerits = ((carSpeed - speedLimit) / excessSpeedPerDemerit);
+                int demerits = ((factualCarSpeed - speedLimit) / excessSpeedPerDemerit);
 
                 if (demerits <= 0)
                     return $"The speed limit is {speedLimit} Son. You are good.";
                 else if (demerits < 12)
                     return ($"The speed limit is {speedLimit} Son. {demerits} demerits");
                 else
-                    return ($"Son, the speed limit is {speedLimit}. That's {demerits} demerits. Slap that lisence right into this hand, eh!.");
+                    return ($"Son, the speed limit is {speedLimit}. That's {demerits} demerits. Your lisence, this hand, eh!.");
             }
         }
     }
