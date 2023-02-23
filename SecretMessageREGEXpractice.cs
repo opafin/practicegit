bc47a444e16cb23e945689e60a2e6c23d261ac1b
OlliRepo <122178992+OpaFin@users.noreply.github.com>
2023-02-23 12:32:30 +0200

    practiceGit 23.2

---
+++ SecretMessageREGEXpractice.cs
@@ -0,0 +1,25 @@
+using System.Text.RegularExpressions;
+
+public class SecretMessage
+{
+    string decryptorPattern = @"(\d{2,3})";
+
+    void Decryptor()
+    {
+        Regex regex = new Regex(decryptorPattern);
+        var input = File.ReadAllLines("input2.txt");
+
+
+        foreach (var line in input)
+        {
+            MatchCollection matchCollection = regex.Matches(line);
+
+            foreach (var match in matchCollection)
+            {
+                string? matchedToString = match.ToString();
+                int matchedToInt = Convert.ToInt32(matchedToString);
+                Console.Write((char)matchedToInt);
+            }
+        }
+    }
+}
